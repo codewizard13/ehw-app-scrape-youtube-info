f96a0769168bda01803fa3a3658b7c8a3e3ccd10
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-27 08:16:00 -0500

    UPDATE app.js:  REMOVE header boilerplate comment

--- app.js
+++ app.js
@@ -1,38 +1,4 @@
-// app.js
-
-/*
-Program Name:   YouTube Information Grabber (Vanilla JS)
-File Name:      ehCode_2018.06.13_JavaScriptES6_GetYouTubeInfo_01.js
-Date Created:   06/13/18
-Date Modified:  10/25/21
-Version:        2.0
-Programmer:     Eric Hepperle
-
-Purpose: Grabs (scrapes) and prints to console title, url, and channel name
-    using vanilla JavaScript ES6.
-    
-    This is useful for quickly grabbing enough information to make a link
-    to a YouTube video and/or channel.
-
-Usage: Cut and paste into developer console window (F12) of a YouTube
-    video post. Vertical newline-separated list of data will output
-    to console.
-
-Sample results: 
-
-    Tutorial Info:
-    - Title: How to Create Photo Gallery Grid with Modal Window Lightbox
-    - URL: https://www.youtube.com/watch?v=IPVQeVlw2_E
-    - Channel: LearnWebCode
-
-Requires:
-  * Browser opened to a YouTube video.
-    
-Demonstrates:
-    * Vanilla JavaScript
-    * JavaScript ES6
-
-*/
+// app.js | Eric Hepperle
 
 console.clear();
 
